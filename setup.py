ffc893017f78d0695ceccc5e9827ed0796305010
Iury O. G. Figueiredo <last.src@gmail.com>
2020-11-26 14:26:23 -0300

    Updating.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 setup(name="websnake",
-      version="2.2.0",
+      version="3.0.0",
       py_modules=['websnake'],
       author="Iury O. G. Figueiredo",
       author_email="ioliveira@id.uff.br",
